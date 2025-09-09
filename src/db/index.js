eab5daeb990b43fa69f999f9c2e2d2ddcbf64e59
LepharamRamchiary <lepharamchiary@gmail.com>
2025-09-09 14:22:58 +0530

    changes

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       `\n mongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
